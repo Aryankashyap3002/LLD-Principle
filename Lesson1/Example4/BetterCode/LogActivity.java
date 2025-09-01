c201302a2b674d38cb0868ea72917a3c4c786d03
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-09-01 14:11:02 +0530

    Added Lession 1 activity 4 solution

---
+++ Lesson1/Example4/BetterCode/LogActivity.java
@@ -0,0 +1,7 @@
+package Lesson1.Example4.BetterCode;
+
+public class LogActivity {
+    public void logActivity () {
+        
+    }
+}
---
+++ Lesson1/Example4/BetterCode/UserManager.java
@@ -0,0 +1,13 @@
+package Lesson1.Example4.BetterCode;
+
+public class UserManager {
+    public void createUser( String username) {
+
+    }
+
+    public void deleteUser( String username ) {
+
+    }
+
+    // as both the operations affects same table
+}
